8220d8aebd57fd0faecb4ee89c8309257d5ada4b
Robin-Verma2005 <ravinderkv1877@gmail.com>
2025-11-05 12:16:19 +0530

    Added STATIC_ROOT setting for deployment

--- timebank/settings.py
+++ timebank/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
+import os
 
 from pathlib import Path
 
@@ -141,6 +142,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Optional: agar tu local run me bhi static use kar raha hai
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
